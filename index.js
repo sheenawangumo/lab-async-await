15aad1f73f0ec6fcbddfd953905fc31aac4c0919
sheenawangumo <sheenawangumo.contact@gmail.com>
2026-04-19 11:31:39 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code here!
+function displayPosts(posts) {
+
+    const postList = document.getElementById('post-list');
+    posts.forEach(post => {const li = document.createElement('li');
+
+    const h1 = document.createElement('h1');
+    h1.textContent = post.title;
+
+    const p = document.createElemnt ('p')
+    p.textcontent = post.body;
+
+    li.appendChild("h1");
+    li.appendChild('p');
+    postList.appendChild('li');
+    }
+   );
+}
+
+async function fetchAndDisplay(call_displayPosts) {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const data = await response.json ();
+    }
+    catch(error){
+        console.log('Oops, something went wrong:', error);
+    }
+ call_displayPosts()   
+}
+
+fetchAndDisplay(displayPosts);
+
